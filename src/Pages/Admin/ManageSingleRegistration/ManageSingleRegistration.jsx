eed4b8c300298c191adbfe891bde39af23c285e8
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-02-06 16:30:21 +0600

    Show ID card button added

--- src/Pages/Admin/ManageSingleRegistration/ManageSingleRegistration.jsx
+++ src/Pages/Admin/ManageSingleRegistration/ManageSingleRegistration.jsx
@@ -417,6 +417,14 @@ const ManageSingleRegistration = () => {
         >
           <button>Modify</button>
         </Link>
+        {participant?.status === "Unpaid" && (
+          <Link
+            to={`/idcard/${participant?.participantId}`}
+            className="text-white ml-3 mb-5 bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none font-medium rounded text-xs md:text-sm lg:text-lg md:px-5 px-2.5 py-1.5 text-center md:py-2.5"
+          >
+            <button>Show ID Card</button>
+          </Link>
+        )}
       </div>
     </div>
   );
